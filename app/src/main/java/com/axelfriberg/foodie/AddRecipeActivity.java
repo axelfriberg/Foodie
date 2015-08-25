995d536bc591e829bf73177b147362fe68cd16c8
Axel Friberg <mail@axelfriberg.com>
2015-08-25 20:36:57 +0200

    Started work on camera functionality.

--- app/src/main/java/com/axelfriberg/foodie/AddRecipeActivity.java
+++ app/src/main/java/com/axelfriberg/foodie/AddRecipeActivity.java
@@ -3,11 +3,18 @@ package com.axelfriberg.foodie;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,6 +25,8 @@ public class AddRecipeActivity extends Activity {
     private EditText mTitleEditText;
     private EditText mInstructionsEditText;
     private FileUtilities fileUtilities;
+    private File f;
+    protected static final int CAPTURE_IMAGE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class AddRecipeActivity extends Activity {
         setContentView(R.layout.activity_add_recipe);
         recipe = new Recipe();
         fileUtilities = new FileUtilities(this);
+        f = new File(getFilesDir()+"/"+"mypic.jpg");
 
         mTitleEditText = (EditText)findViewById(R.id.title_EditText);
         mInstructionsEditText = (EditText) findViewById(R.id.instructions_EditText);
@@ -47,39 +57,98 @@ public class AddRecipeActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_settings:
+                return true;
+            case R.id.done_button:
+                save();
+                return true;
+            case android.R.id.home:
+                backSaveDialog();
+                return true;
+            case R.id.camera_button:
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                AddRecipeActivity.this.startActivityForResult(i, CAPTURE_IMAGE_REQUEST_CODE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        if (id == R.id.done_button){
-            save();
-            return true;
-        }
-
-        if(id == android.R.id.home){
-            new AlertDialog.Builder(this)
-                    .setMessage("Do you want to save the current recipe?")
-                    .setCancelable(false)
-                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            save();
-                        }
-                    })
-                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            finish();
-                        }
-                    })
-                    .show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
-        backCheck();
+        backSaveDialog();
+    }
+
+    /* Se till så att vi visar bilden även efter texrotation. Behöver veta
+	 * storleken på ImageViewn
+	 * Därför gjort detta i denna metod istället för onCreate tex då
+	 * detta värde inte finns tillgängligt då
+	 */
+    //
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(f.exists()&&hasFocus)
+            updateImageViewFromFile();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
+            Log.d("AddRecipe", Integer.toString(resultCode));
+            if (resultCode == RESULT_OK) {
+                updateImageViewFromFile();
+                Log.d("AddRecipe", "ok");
+            } else if (resultCode == RESULT_CANCELED) {
+                // User did not want to take a picture
+                Log.d("AddRecipe", "canceled");
+            } else {
+                // Something went wrong
+                Log.d("AddRecipe", "error");
+            }
+        }
+
+    }
+
+    private void updateImageViewFromFile() {
+        ImageView imView=(ImageView) findViewById(R.id.imageView_add);
+
+        //Läs in bilden som nu bör finnas där vi sa att den skulle placeras
+        Bitmap bm = BitmapFactory.decodeFile(f.getAbsolutePath());
+        Log.d("AddRecipe", f.getAbsolutePath());
+
+        //Skala om bilden så att den passar i imageviewn
+        Bitmap bm2 = Bitmap.createScaledBitmap(bm,
+                imView.getWidth(),
+                imView.getHeight(),
+                true);
+        imView.setImageBitmap(bm2);
+    }
+
+    private boolean fileExists(File[] files, String s){
+        for(File f : files){
+            if (f.getName().compareTo(s) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void backSaveDialog(){
+        new AlertDialog.Builder(this)
+                .setMessage("Do you want to save the current recipe?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        save();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .show();
     }
 
     private void save(){
@@ -97,32 +166,6 @@ public class AddRecipeActivity extends Activity {
             finish();
         }
 
-    }
-
-    private boolean fileExists(File[] files, String s){
-        for(File f : files){
-            if (f.getName().compareTo(s) == 0){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void backCheck(){
-        new AlertDialog.Builder(this)
-                .setMessage("Do you want to save the current recipe?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        save();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                })
-                .show();
     }
 
 
